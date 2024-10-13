6a7818156950bf6b09258fc78bab4e2b997bc531
JonBorka <82261044+Jonboyy@users.noreply.github.com>
2024-10-14 00:10:32 +0200

    Delete SolarWatch copy directory

--- SolarWatch copy/SolarWatch/Models/GeocodingResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace SolarWatch.Models
-{
-    // Model for the Geocoding API response containing latitude and longitude
-    public class GeocodingResponse
-    {
-        public double Lat { get; set; } // Note: This should match the exact field names in the JSON
-        public double Lon { get; set; }
-    }
-}
--- SolarWatch copy/SolarWatch/Models/SunriseSunsetResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace SolarWatch.Models
-{
-    // Model for the Sunrise/Sunset API response containing only sunrise and sunset times
-    public class SunriseSunsetResponse
-    {
-        public DateTime Sunrise { get; set; } // Sunrise time in local city time
-        public DateTime Sunset { get; set; }  // Sunset time in local city time
-    }
-}
